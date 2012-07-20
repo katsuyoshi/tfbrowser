ce5287eb8abbc71cb830da8ff95f9f0e1b29bbb1
Katsuyoshi Ito <kito@itosoft.com>
2012-07-20 21:59:59 +0900

    added browser and unlock screen

--- TFBrowser/AppDelegate.m
+++ TFBrowser/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CIALBrowserViewController.h"
 
 @implementation AppDelegate
 
@@ -15,7 +16,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    application.idleTimerDisabled = YES;
+
+    CIALBrowserViewController *controller = [CIALBrowserViewController new];
+    controller.url = [NSURL URLWithString:@"http://tfree.jp/"];
+    
+    self.window.rootViewController = controller;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
