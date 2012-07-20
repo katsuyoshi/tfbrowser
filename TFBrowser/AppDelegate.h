2e60b8a1720fcedac6be5b8a5571f863ae5fda5a
Katsuyoshi Ito <kito@itosoft.com>
2012-07-20 21:23:03 +0900

    Initial Commit

---
+++ TFBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TFBrowser
+//
+//  Created by Ito Katsuyoshi on 12/07/20.
+//  Copyright (c) 2012年 ITO SOFT DESIGN Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
